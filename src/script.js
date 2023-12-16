bff4fa4a83c4112aaa61c995896b22fdc68f718f
JoyEmma12 <ejoy0600@gmail.com>
2023-12-16 13:35:53 +0100

    added function and setInterval

--- src/script.js
+++ src/script.js
@@ -1,18 +1,19 @@
 //console.log("name");
 //document.getElementById("times").innerHTML = "my name is";
+function displayClock() {
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+  minutes = checkTime(minutes);
+  seconds = checkTime(seconds);
+  let display = document.getElementById("times");
+  display.innerHTML = ` ${hours}   : ${minutes}   :   ${seconds}`;
 
-let today = new Date();
-let hours = today.getHours();
-let minutes = today.getMinutes();
-let seconds = today.getSeconds();
-minutes = checkTime(minutes);
-seconds = checkTime(seconds);
-let display = document.getElementById("times");
-display.innerHTML = ` ${hours}   : ${minutes}   :   ${seconds}`;
-let t = setTimeout(500);
-document.getElementById("times").style.fontSize = "50px";
-document.getElementById("times").style.fontWeight = "700";
-
+  document.getElementById("times").style.fontSize = "50px";
+  document.getElementById("times").style.fontWeight = "700";
+}
+setInterval(displayClock, 10);
 function checkTime(i) {
   if (i < 10) {
     i = "0" + i;
